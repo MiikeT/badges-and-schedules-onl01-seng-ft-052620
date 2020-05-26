800d061a16f63ec49676a7469bc3e5386f569a80
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 20:14:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(speakers)
   rooms = [1,2,3,4,5,6,7]
-  attendees.each_with_index do |speaker, i|
+  attendees.map_with_index do |speaker, i|
     return "Hello, #{speaker}! You'll be assigned to room #{room[i]}."
   end
 end
