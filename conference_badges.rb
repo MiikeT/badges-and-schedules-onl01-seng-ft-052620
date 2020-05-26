aa9ca17d5365e3b013ed9b3598600f255f3e5bd4
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 20:13:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(names)
   badges = names.map { |name| badge_maker(name) }
   badges
 end
+
+
+def assign_rooms(speakers)
+  rooms = [1,2,3,4,5,6,7]
+  attendees.each_with_index do |speaker, i|
+    return "Hello, #{speaker}! You'll be assigned to room #{room[i]}."
+  end
+end
